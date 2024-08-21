c2d51cc7a84aa927b290fc92f60c12f5cb23e863
WhoSowSee <yanmishbash.i.s.2.21@gmail.com>
2024-08-21 23:51:54 +0300

    Added new settings: FlowLauncher, Goland, ShareX

---
+++ FlowLauncher/Plugins/Youtube Downloader-0.1.2/main.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import sys, os
+parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(parent_folder_path)
+sys.path.append(os.path.join(parent_folder_path, 'lib'))
+sys.path.append(os.path.join(parent_folder_path, 'plugin'))
+from flowlauncher import FlowLauncher
+from pytube import YouTube
+from pathlib import Path
+
+
+class YoutubeDownload(FlowLauncher):
+    
+    def is_valid_url(self, url):
+        try:
+            youtube = YouTube(url)
+            return True
+        except:
+            return False
+
+
+    def query(self, query):
+        self.queryd = query
+
+        # Cases:
+        # Empty query
+        if not query:
+            return [
+            {
+                "Title": "Where's your URL?",
+                "SubTitle": "Please, paste a YouTube URL!",
+                "IcoPath": "Images/question.png"
+            }
+        ]
+            
+        # Invalid URL
+        elif not self.is_valid_url(query):
+            return [
+                {
+                    "Title": "Invalid URL!", 
+                    "SubTitle": "Please, check if the URL or video is available.",
+                    "IcoPath": "Images/stop.png"
+                }
+            ]
+
+        # Valid URL
+        return [
+            # Audio option
+            {
+                "Title": "Download the audio from your video",
+                "SubTitle": "Download the audio in .mp3",
+                "IcoPath": "Images/audio.png",
+                "JsonRPCAction": {
+                    "method": "download_audio",
+                    "parameters": [query]
+                }
+            },
+            # Video option
+            {
+                "Title": "Download your video in full quality",
+                "SubTitle": "Download the video in .mp4",
+                "IcoPath": "Images/video.png",
+                "JsonRPCAction": {
+                    "method": "download_video",
+                    "parameters": [query]
+                }
+            }
+        ]
+
+
+    def context_menu(self, data):
+        return []
+
+    def download_audio(self, url):
+        download_path = str(Path.home() / "Downloads" / "Youtube Downloads"); # Define the Download Path
+        youtube = YouTube(url)
+        video = youtube.streams.filter(only_audio = True).first()
+        audio_file = video.download(output_path = download_path)
+        base, ext = os.path.splitext(audio_file) # Get the file name
+        os.replace(audio_file, base + '.mp3')    # Convert to mp3
+        os.system(f'explorer "{download_path}"')
+
+    def download_video(self, url):
+        download_path = str(Path.home() / "Downloads" / "Youtube Downloads"); # Define the Download Path
+        youtube = YouTube(url)
+        video = youtube.streams.get_highest_resolution()
+        video_file = video.download(output_path = download_path)
+        os.system(f'explorer "{download_path}"')
+
+
+if __name__ == "__main__":
+    YoutubeDownload()
